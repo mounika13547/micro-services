cd4a604ae9284021357d048715f2161db21c5f42
mounika-mogaparthi <mounika3b4u@gmail.com>
2024-07-16 19:47:41 -0400

    Initial setup

---
+++ src/main/java/com/mounika/springboot/SpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.mounika.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+}
